f01bb30b4f13fe204bb38be14352cf9f1c722589
Cjessiel <62605235+Cjessiel@users.noreply.github.com>
2023-03-30 13:27:51 -0300

    Criando classe que fará a leitura e a escrita dos dados

--- src/main/java/org/example/MainApplication.java
+++ src/main/java/org/example/MainApplication.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainApplication {
     public static void main(String[] args) {
 
+        final String pathOut = "src/HOMEPATH/data/out/dataOut.txt";
 
+        while (true) {
+            List<String> finalData = new ArrayList<>();
+            ReadAndWriter readAndWriter = new ReadAndWriter();
+
+
+        }
     }
 }
